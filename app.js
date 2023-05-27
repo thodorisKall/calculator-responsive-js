4aadc7d031320a3c40b904a5bafc1196fba25a85
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-05-28 02:20:26 +0300

    feat: complete basic calculate functionality for the app

---
+++ app.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll(".btn")
+const screen = document.querySelector(".display__text")
+const deleteAll = document.getElementById("delete-all")
+
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    calculate(button)
+  })
+)
+
+let typedNumbers = []
+
+function calculate(button) {
+  let result
+  let rawNumber = button.innerText
+
+  if (rawNumber === "AC") {
+    typedNumbers = []
+    console.log("Cleared")
+  } else if (button.id === "delete-one") {
+    typedNumbers.pop()
+    console.log("Deleted one")
+  } else if (rawNumber === "=") {
+    result = eval(typedNumbers.join(""))
+    console.log("Calculated")
+  } else {
+    typedNumbers.push(rawNumber)
+    result = typedNumbers.join("")
+  }
+  display(result)
+}
+
+function display(result) {
+  if (result === undefined) {
+    screen.innerText = ""
+  } else {
+    screen.innerText = result
+  }
+}
